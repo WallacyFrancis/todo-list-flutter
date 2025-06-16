ef531575ac734a8ba31fea7771cab79f0a09b01d
WallacyFrancis <wallacy_francis@hotmail.com>
2025-06-16 02:23:51 +0000

    Finalizado meu aplicativo de to-do list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/todo_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'To-DO List App',
+      // Remove a faixa de "Debug" no canto da tela
+      debugShowCheckedModeBanner: false,
+      // Define o tema global do nosso aplicativo
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // Tema escuro
+        brightness: Brightness.dark,
+        // Cor primária (usada na AppBar, por exemplo)
+        primaryColor: const Color(0xFF1e3799),
+        // Cor de fundo do Scaffold (telas)
+        scaffoldBackgroundColor: const Color(0xFF0d1117),
+        // Cor de destaque para botões e outros elementos interativos
+        colorScheme: ColorScheme.dark(
+          primary: Color(0xFF4a69bd),
+          secondary: Color(0xFF4a69bd),
+        ),
+        // Define uma fonte padrão para o app
+        fontFamily: 'Roboto',
+        // Estilo para o FloatingActionButton
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Color(0xFF4a69bd),
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF161b22),
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TodoScreen(),
     );
   }
 }
