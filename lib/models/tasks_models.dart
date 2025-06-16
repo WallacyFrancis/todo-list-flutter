a31559e917348a7919ffdc6639fbe8e9172f2ccc
WallacyFrancis <wallacy_francis@hotmail.com>
2025-06-16 01:26:40 +0000

    Foi definido a minha task de model e o primeiro widgetr para cada task usando a model declarada

---
+++ lib/models/tasks_models.dart
@@ -0,0 +1,11 @@
+class Task {
+  final String id;
+  String title;
+  bool isDone;
+
+  Task({
+    required this.id,
+    required this.title,
+    required this.isDone
+  });
+}
---
+++ lib/widgets/talk_list_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import '../models/tasks_models.dart';
+
+class TaskItem extends StatelessWidget {
+  final Task task;
+  // funções de callback para notificar o widget das alterações
+  final VoidCallback onChanged;
+  final VoidCallback onDelete;
+
+  // construtor dos dados necessários
+  const TaskItem({
+    super.key,
+    required this.task,
+    required this.onChanged,
+    required this.onDelete,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      // Cor de fundo do card, ligeiramente mais clara que o fundo da tela
+      color: const Color(0xFF161b22),
+      margin: EdgeInsets.symmetric(vertical: 6.2, horizontal: 8.0),
+      elevation: 3,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: ListTile(
+        // O Checkbox à esquerda
+        leading: Checkbox(
+          value: task.isDone, 
+          onChanged: (value) => onChanged(),
+          activeColor: Colors.green,
+        ),
+        // O título da tarefa
+        title: Text(
+          task.title,
+          style: TextStyle(
+            // Aplica o estilo de texto riscado se a tarefa estiver concluída
+            decoration: task.isDone ? TextDecoration.lineThrough : TextDecoration.none,
+            color: task.isDone ? Colors.grey : Colors.white,
+            fontSize: 16
+          ),
+        ),
+          // O botão de deletar à direita
+        trailing: IconButton(
+          icon: const Icon(Icons.delete_outline, color: Colors.redAccent),
+          onPressed: onDelete, // Chama o callback de deleção ao ser pressionado
+        ),
+        onTap: onChanged, // Permite clicar em qualquer lugar do item para marcar/desmarcar
+      ),
+    );
+  }
+}
